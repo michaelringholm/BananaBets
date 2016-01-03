5a6598830856425830ae8656327875dc30aa867b
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2016-01-03 11:18:01 +0100

    First checkin

---
+++ html/resources/js/banana/main.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+    $("#dialog-modal").dialog({
+        height: 140,
+        modal: true,
+        autoOpen: false,
+        draggable: false
+    });
+
+    $(".betValue").click(function (e) { showOpenBetDialog(e.currentTarget); });
+});
+
+function showOpenBetDialog(e)
+{
+    $("#selectedBetValue").text($(e).attr("data-bet-value"));
+    $("#selectedBetTitle").text($(e).attr("data-bet-title"));
+    $("#selectedBetOutcome").text($(e).attr("data-bet-outcome"));
+ 
+    $("#dialog-modal").dialog({ width: 500, height: 600 });
+    $("#dialog-modal").dialog("open");
+
+    $(".inviteAction").click(function (e) { inviteUninviteFriend(e.currentTarget); });
+}
+
+function inviteUninviteFriend(e)
+{
+    if($(e).text() == "Invitér")
+        $(e).text("Fjern");
+    else
+        $(e).text("Invitér");
+}
